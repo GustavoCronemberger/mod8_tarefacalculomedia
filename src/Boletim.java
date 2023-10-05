297d0ae42bbb854ba8e664b293255e5d4af60a65
Gustavo Cronemberger <gustavocronemberger@gmail.com>
2023-10-05 15:41:02 -0300

    Cálculo da Média Final de um Aluno no Ensino Médio

---
+++ src/Boletim.java
@@ -0,0 +1,20 @@
+public class Boletim {
+    public static void main (String aegs[]){
+        mediaFinal();
+
+    }
+
+    private static void mediaFinal(){
+        System.out.println("Boletim: 4º Bimestre");
+
+        int nota1 = 8;
+        int nota2 = 7;
+        int nota3 = 9;
+        int nota4 = 6;
+
+        int mediaF = (nota1+nota2+nota3+nota4)/4;
+        System.out.println("Nota: " + mediaF);
+
+    }
+
+}
